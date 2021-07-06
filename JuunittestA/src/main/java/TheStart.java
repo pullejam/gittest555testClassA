2b44ccd351ca91e2bc6bc8d02e110ad84d3805c6
pullejam <79878433+pullejam@users.noreply.github.com>
2021-07-06 09:57:31 -0600

    jjjjj

---
+++ JuunittestA/src/main/java/TheStart.java
@@ -0,0 +1,12 @@
+
+public class TheStart {
+
+	public static void main(String[] args) {
+		int intReturn;
+		Calculator clsCalc = new Calculator();
+		intReturn = clsCalc.add(4, 5);
+		System.out.println(intReturn);
+
+	}
+
+}
